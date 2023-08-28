ad9a6bba8b04823ac2a10883a85934718593ec99
Sergei Pokhorukov <tbksap@gmail.com>
2023-08-28 17:26:11 +0300

    Add file with exceptions

--- app/users/dependencies.py
+++ app/users/dependencies.py
@@ -1,8 +1,9 @@
 from datetime import datetime
 from jose import jwt, JWTError
-from fastapi import Depends, HTTPException, Request, status
+from fastapi import Depends,  Request
 
 from app.config import settings
+from app.exceptions import IncorrectTokenFormatException, TokenAbsentException, TokenExpiredException, UserInNotPresentException
 from app.users.dao import UsersDAO
 
 
@@ -10,7 +11,7 @@ from app.users.dao import UsersDAO
 def get_token(request:Request):
     token = request.cookies.get("booking_access_token")
     if not token:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+        raise TokenAbsentException
     return token
     
 
@@ -22,21 +23,21 @@ async def get_current_user(token: str = Depends(get_token)):
             token, settings.SECRET_KEY, settings.ALGORITHM
         )
     except JWTError:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+        raise IncorrectTokenFormatException
     
     # Проверка времени действия токена
     expire: str = payload.get("exp")
     if (not expire) or (int(expire) < datetime.utcnow().timestamp()): 
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+        raise TokenExpiredException
     
     # Проверка в данных токена параметра sub, в который записывали id юзера
     user_id: str = payload.get("sub")
     if not user_id:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+        raise UserInNotPresentException
     
     # Запрос в БД по id юзера и возврат юзера из БД
     user = await UsersDAO.find_by_id(int(user_id))
     if not user:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+        raise UserInNotPresentException
     
     return user
