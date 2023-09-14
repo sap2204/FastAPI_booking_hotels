86c477b741d27a98f0a0e0b536d282a5941bf563
Sergei Pokhorukov <tbksap@gmail.com>
2023-09-14 18:40:28 +0300

    Add temlate for rooms. Add endpoint get_rooms_hotel_page

--- app/images/router.py
+++ app/images/router.py
@@ -8,6 +8,7 @@ router = APIRouter(
 )
 
 
+# Эндпоинт для загрузки картинок
 @router.post("/hotels")
 async def add_hotel_image(name: int, file: UploadFile):
     with open(f"app/static/images/{name}.webp", "wb+") as file_object:
--- app/pages/router.py
+++ app/pages/router.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Depends, Request
 from fastapi.templating import Jinja2Templates
 
 from app.hotels.router import get_hotels_location_dates
+from app.rooms.router import get_rooms_from_hotel
 
 
 router = APIRouter(
@@ -13,6 +14,7 @@ router = APIRouter(
 templates = Jinja2Templates(directory="app/templates")
 
 
+# Эндпоинт для графического отображения списка отелей
 @router.get("/hotels")
 async def get_hotels_page(
     request: Request,
@@ -21,5 +23,17 @@ async def get_hotels_page(
     return templates.TemplateResponse(
         name="hotels.html",
          context={"request": request, "hotels": hotels})
+
+
+# Эндпоинт для графического отображения номеров отеля
+@router.get("/rooms")
+async def get_rooms_hotel_page(
+    request: Request,
+    rooms = Depends(get_rooms_from_hotel)
+):
+    return templates.TemplateResponse(
+        name="rooms.html",
+        context={"request": request, "rooms": rooms})
+    
 
 
