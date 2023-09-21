007b628133417da47fdc373d743a93d38d5d0a21
Sergei Pokhorukov <tbksap@gmail.com>
2023-09-21 22:07:54 +0300

    Start to work with pytest. Correct database.py, config.py

--- app/config.py
+++ app/config.py
@@ -1,19 +1,38 @@
+from typing import Literal
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
+    MODE: Literal["DEV", "TEST", "PROD"]
+
     DB_HOST: str
     DB_PORT: int
     DB_USER: str
     DB_PASS: str
     DB_NAME: str
 
-    # возвращение адреса ДБ
+    # возвращение адреса БД
     @property
     def get_database_url(self):
         return (f'postgresql+asyncpg://{self.DB_USER}:'
                 f'{self.DB_PASS}@{self.DB_HOST}:'
                 f'{self.DB_PORT}/{self.DB_NAME}'
+        )
+    
+
+    # Данные для тестовой БД
+    TEST_DB_HOST: str
+    TEST_DB_PORT: int
+    TEST_DB_USER: str
+    TEST_DB_PASS: str
+    TEST_DB_NAME: str
+
+    # возвращение адреса тестовой БД
+    @property
+    def get_test_database_url(self):
+        return (f'postgresql+asyncpg://{self.TEST_DB_USER}:'
+                f'{self.TEST_DB_PASS}@{self.TEST_DB_HOST}:'
+                f'{self.TEST_DB_PORT}/{self.TEST_DB_NAME}'
         )
 
     # указание файла с закрытыми данными для подключения к ДБ
--- app/database.py
+++ app/database.py
@@ -1,13 +1,22 @@
 # Подключение к БД
 
+from sqlalchemy import NullPool
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
 from app.config import settings
 
-DATABASE_URL = settings.get_database_url
 
-engine = create_async_engine(DATABASE_URL)
+# Формирование адреса БД в зависимости от режимам подключения
+if settings.MODE == "TEST":
+    DATABASE_URL = settings.get_test_database_url
+    DATABASE_PARAMS = {"poolclass" : NullPool}
+else:
+    DATABASE_URL = settings.get_database_url
+    DATABASE_PARAMS = {}
+
+
+engine = create_async_engine(DATABASE_URL, **DATABASE_PARAMS)
 
 async_session_maker = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
