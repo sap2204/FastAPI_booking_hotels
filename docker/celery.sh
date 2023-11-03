500d8a66bb158c6b0f82ef617d483ab6e05654a2
sap2204 <tbksap@gmail.com>
2023-11-03 15:30:24 +0300

    Add file docker-compose.yml

---
+++ docker/celery.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [["${1}" == "celery" ]]; then 
+    celery --app=app.tasks.celery_setup:celery worker -l INFO
+elif [["${1}" == "flower" ]]; then 
+    celery --app=app.tasks.celery_setup:celery flower
+fi
+
